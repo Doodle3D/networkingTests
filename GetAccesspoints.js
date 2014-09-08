0251581c76ea531028ecca271eea9dbcf64b111c
Doodle3D <info@doodle3d.com>
2014-09-08 15:03:30 +0000

    create AP, list networks, disconnect AP, connect to network, disconnect from network

--- GetAccesspoints.js
+++ GetAccesspoints.js
@@ -9,6 +9,8 @@ var ConnMan = require('jsdx-connman');
 var async = require('async');
 var Q = require('q');
 var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+var ps = require('ps-node');
 
 //from: http://stackoverflow.com/questions/8556777/dbus-php-unable-to-launch-dbus-daemon-without-display-for-x11
 process.env.DISPLAY = ':0';
@@ -18,46 +20,60 @@ var connman = new ConnMan();
 
 connman.init(function() {
 
-	createAccesspoint(function(err, res) {
+	enableHotspot(function(err, res) {
 		if(!err) {
 			console.log('RES: ' + res);
+
+			console.log('timeout...');
+			setTimeout(test, 10000, function(err, res) {
+				if(!err) {
+					console.log('test() res');
+				}
+			});
 		} else {
 			console.log('ERR: ' + err);
 		}
 	})
 
-	// getAccesspoints(function(err, list) {
-	// 	if(!err) {
-	// 		for (var index in list) {
-	// 			var ap = list[index];
-	// 			var name = String((ap.Name ? ap.Name : '*hidden*'));
-	// 			console.log('  ' + name, '  \t\t\t Strength: ' + ap.Strength + '%', '  \t\t\t Security: ' + ap.Security);
-	// 		}
+	function test(callback) {
+		getAccesspoints(function(err, list) {
+			if(!err) {
+				for (var index in list) {
+					var ap = list[index];
+					var name = String((ap.Name ? ap.Name : '*hidden*'));
+					console.log('  ' + name, '  \t\t\t Strength: ' + ap.Strength + '%', '  \t\t\t Security: ' + ap.Security);
+				}
 
-	// 		connectToAccesspoint('Doodle3D-wisp', '', function(err, res) {
-	// 			if(!err) {
-	// 				console.log('callback res: ' + res);
+				disableHotspot(function(err,res) {
+					if(!err) { 
+						console.log("res: " + res);
+					}
 
-	// 				console.log('timeout to disconnect...')
-	// 				setTimeout(disconnectFromAccesspoint, 10000, 'Doodle3D-wisp', function(err, res) {
-	// 					if(!err) {
-	// 						console.log("res: " + res);
-	// 					} else {
-	// 						console.log("err: " + err);
-	// 					}
-	// 				});
-	// 			} else {
-	// 				console.log('ERR ' + err);
-	// 			}
-	// 		})
-	// 	} else {
-	// 		console.log("error: " + err);
-	// 	}
-	// })
-	
+					connectToAccesspoint('Doodle3D-wisp', '', function(err, res) {
+					if(!err) {
+						console.log('callback res: ' + res);
+
+						console.log('timeout to disconnect...')
+						setTimeout(disconnectFromAccesspoint, 10000, 'Doodle3D-wisp', function(err, res) {
+							if(!err) {
+								console.log("res: " + res);
+							} else {
+								console.log("err: " + err);
+							}
+						});
+						} else {
+							console.log('ERR ' + err);
+						}
+					})
+				})
+			} else {
+				console.log("error: " + err);
+			}
+		})
+	};	
 });
 
-createAccesspoint = function(cb) {
+enableHotspot = function(cb) {
 	/* NOTE
 	 * http://serverfault.com/questions/480812/privileges-for-ifconfig
 	 * You do not need root access to use ifconfig to change IP addresses, only CAP_NET_ADMIN.
@@ -66,18 +82,11 @@ createAccesspoint = function(cb) {
 	 * $./ifconfig eth0 1.2.3.4    # succeeds
 	 */
 
-	// cb(null, 'created accesspoint');
-
-	//TODO
-	//ps aux | grep dnsmasq
-	//kill <pid>
-
-	var wlanId = 'wlan3';
+	var wlanId = 'wlan0';
 
 	async.series
     ([  
-        function (callback)
-        {
+        function (callback) {
             console.log('bring '+wlanId+' up...');
             //note: ./ifconfigUser is local copy of ifconfig with previleges
             var child = exec('./ifconfigUser '+wlanId+' up');
@@ -92,8 +101,7 @@ createAccesspoint = function(cb) {
 			})
         }
         ,
-        function (callback)
-        {
+        function (callback) {
             console.log('set '+wlanId+' ip...');
             var child = exec('./ifconfigUser '+wlanId+' 192.168.1.1');
 			child.stdout.on('data', function(data) { 
@@ -107,31 +115,29 @@ createAccesspoint = function(cb) {
 			})
         }
         ,
-        function (callback)
-        {
-        	//TODO: FIRST KILL DNSMASQ BY PS AUX | GREP DNSMASQ , KILL <PID>
+        /* NOTE: dnsmasq is started at boot */
+   //     	,
+   //      function (callback) {
+   //      	//TODO: FIRST KILL DNSMASQ BY PS AUX | GREP DNSMASQ , KILL <PID>
+   //          console.log("dnsmasq start...");
+   //          var child = exec('dnsmasq -i '+wlanId+' -F 192.168.1.100,192.168.1.200,12h');
+			// child.stdout.on('data', function(data) { 
+			// 	console.log('stdout: ' + data); 
+			// })
+			// child.stderr.on('data', function(data) { 
+			// 	console.log('stdout: ' + data); 
+			// })
+			// child.on('close', function(code) {
+			// 	callback();
+			// })
+   //      }
+   //      ,
+        function (callback) {
+            console.log("hostapd start...");
+			
+			//-B: run as deamon in background, -t:time log messages, -d: debug messages			
+			var child = spawn('hostapd', ['-B', '-t', '-d', '/etc/hostapd/hostapd.conf'], {});
 
-            console.log("dnsmasq...");
-            var child = exec('dnsmasq -i '+wlanId+' -F 192.168.1.100,192.168.1.200,12h');
-			child.stdout.on('data', function(data) { 
-				console.log('stdout: ' + data); 
-			})
-			child.stderr.on('data', function(data) { 
-				console.log('stdout: ' + data); 
-			})
-			child.on('close', function(code) {
-				callback();
-			})
-        }
-        ,
-        function (callback)
-        {
-        	//TODO: RUN HOSTAPD IN BACKGROUND
-        	//TODO: MAKE SPAWN?
-        	//TODO: call callback at right moment based on stdout content
-
-            console.log("hostapd...");
-            var child = exec('hostapd /etc/hostapd/hostapd.conf &');
 			child.stdout.on('data', function(data) { 
 				console.log('stdout: ' + data); 
 			})
@@ -140,7 +146,7 @@ createAccesspoint = function(cb) {
 			})
 			child.on('close', function(code) {
 				// callback();
-				cb(null, 'created accesspoint');
+				cb(null, 'hotspot enabled');
 			})
         }
     ]);
@@ -154,6 +160,35 @@ createAccesspoint = function(cb) {
 	// ./ifconfig wlan3 192.168.1.1
 	// dnsmasq -i wlan3 -F 192.168.1.100,192.168.1.200,12h -p 55
 	// hostapd /etc/hostapd/hostapd.conf
+}
+
+disableHotspot = function(cb) {
+
+	// Technologies
+	connman.getTechnologies(function(err, technologies) {
+		//--see if wifi module exists
+		if(technologies.WiFi !== undefined) {
+			console.log("wifi module exists");
+
+			//--make sure wifi module is enabled (powered)
+			technologies.WiFi.getProperties(function(err, properties) {
+				if(!err) {
+					var wifi = connman.technologies['WiFi'];
+
+					if(properties.Powered) {
+						wifi.setProperty('Powered', false, function(err, res) {
+							console.log('Disabled wifi...');
+							console.log('small timeout for wifi module to power down...')
+							setTimeout(cb, 5000, null, 'wifi disabled');
+						});
+					} else {
+						console.log('wifi was already disabled');
+						cb(null, 'wifi already disabled');
+					}
+				}
+			});
+		}
+	});
 }
 
 getAccesspoints = function(cb) {
@@ -218,98 +253,127 @@ getAccesspoints = function(cb) {
 }
 
 connectToAccesspoint = function(serviceName, passphrase, cb) {
-	var wifi = connman.technologies['WiFi'];
 
-	wifi.findAccessPoint(serviceName, function(err, service) {
-		//--check if service name exists
-		if (!service) {
-			cb(new Error('No such accesspoint: ' + serviceName));
-			return;
-		} else {
-			console.log('Try connecting to service: ' + service.serviceName);
-		}
+	// Technologies
+	connman.getTechnologies(function(err, technologies) {
+		//--see if wifi module exists
+		if(technologies.WiFi !== undefined) {
+			console.log("wifi module exists");
 
-		//--get connection
-		connman.getConnection(service.serviceName, function(err, ap) {
+			//--make sure wifi module is enabled (powered)
+			technologies.WiFi.getProperties(function(err, properties) {
+				if(!err) {
+					var wifi = connman.technologies['WiFi'];
 
-			/* Making connection to access point */
-			ap.connect(function(err, agent) {
-
-				if (err) {
-					cb(err);
-					return;
-				} else {
-					// cb(agent);
+					if(!properties.Powered) {
+						wifi.setProperty('Powered', true, function(err, res) {
+							console.log('Enabled wifi');
+							console.log('small timeout for wifi module to get powered...')
+							setTimeout(connect, 5000); //10000ms = 10 sec = delay for antenna hardware to power up
+						});
+					} else {
+						console.log('wifi was already enabled');
+						connect();
+					}
 				}
+			})
+		}
+	})
 
-				var failed = false;
+	function connect(callback) {
+		var wifi = connman.technologies['WiFi'];
 
-				console.log('Connecting ...');
-				// cb(null, 'Connecting....');
+		wifi.findAccessPoint(serviceName, function(err, service) {
+			//--check if service name exists
+			if (!service) {
+				cb(new Error('No such accesspoint: ' + serviceName));
+				return;
+			} else {
+				console.log('Try connecting to service: ' + service.serviceName);
+			}
 
-				agent.on('Release', function() {
-					console.log('Release');
-				});
+			//--get connection
+			connman.getConnection(service.serviceName, function(err, ap) {
 
-				agent.on('ReportError', function(path, err) {
-					console.log('ReportError:');
-					console.log(err);
-					failed = true;
-					/* connect-failed */
-					/* invalid-key */
-				});
+				/* Making connection to access point */
+				ap.connect(function(err, agent) {
 
-				agent.on('RequestBrowser', function(path, url) {
-					console.log('RequestBrowser');
-				});
-
-				/* Initializing Agent for connecting access point */
-				agent.on('RequestInput', function(path, dict, callback) {
-					console.log(dict);
-
-					if ('Passphrase' in dict) {
-						callback({ 'Passphrase': passphrase });
+					if (err) {
+						cb(err);
 						return;
+					} else {
+						// cb(agent);
 					}
 
-					callback({});
-				});
+					var failed = false;
 
-				agent.on('Cancel', function() {
-					console.log('Cancel');
-				});
+					console.log('Connecting ...');
+					// cb(null, 'Connecting....');
 
-				ap.on('PropertyChanged', function(name, value) {
-					// console.log(name + '=' + value);
+					agent.on('Release', function() {
+						console.log('Release');
+					});
 
-					if (name == 'State') {
-						switch(value) {
-						case 'failure':
-							console.log('Connection failed');
-							break;
+					agent.on('ReportError', function(path, err) {
+						console.log('ReportError:');
+						console.log(err);
+						failed = true;
+						/* connect-failed */
+						/* invalid-key */
+					});
 
-						case 'association':
-							console.log('Associating ...');
-							break;
+					agent.on('RequestBrowser', function(path, url) {
+						console.log('RequestBrowser');
+					});
 
-						case 'configuration':
-							console.log('Configuring ...');
-							break;
+					/* Initializing Agent for connecting access point */
+					agent.on('RequestInput', function(path, dict, callback) {
+						console.log(dict);
 
-						case 'ready':
-							console.log('Ready!')
-							cb(null, 'Ready!');
-							break;
-
-						case 'online':
-							console.log('Connected');
-							break;
+						if ('Passphrase' in dict) {
+							callback({ 'Passphrase': passphrase });
+							return;
 						}
-					}
+
+						callback({});
+					});
+
+					agent.on('Cancel', function() {
+						console.log('Cancel');
+					});
+
+					ap.on('PropertyChanged', function(name, value) {
+						// console.log(name + '=' + value);
+
+						if (name == 'State') {
+							switch(value) {
+							case 'failure':
+								console.log('Connection failed');
+								break;
+
+							case 'association':
+								console.log('Associating ...');
+								break;
+
+							case 'configuration':
+								console.log('Configuring ...');
+								break;
+
+							case 'ready':
+								console.log('Ready!')
+								cb(null, 'Ready!');
+								break;
+
+							case 'online':
+								console.log('Connected');
+								break;
+							}
+						}
+					});
 				});
 			});
 		});
-	});
+	}
 }
 
 disconnectFromAccesspoint = function(serviceName, cb) {
